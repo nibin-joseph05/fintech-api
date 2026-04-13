a014e76f86ec9e7f4ca3dd8ac6221f0c71fe9486
Nibin Joseph <nibin.joseph.career@gmail.com>
2026-04-13 21:45:53 +0530

    designed the user entity

---
+++ src/main/java/com/nibin/fintech/modules/user/entity/User.java
@@ -0,0 +1,39 @@
+package com.nibin.fintech.modules.user.entity;
+
+import com.nibin.fintech.modules.user.entity.enums.Status;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String mobile;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/nibin/fintech/modules/user/entity/enums/Status.java
@@ -0,0 +1,6 @@
+package com.nibin.fintech.modules.user.entity.enums;
+
+public enum Status {
+    PENDING,
+    ACTIVE
+}
