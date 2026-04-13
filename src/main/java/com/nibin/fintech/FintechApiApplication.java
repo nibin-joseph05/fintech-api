280bc256fb8cbfce4f855224565385c984326a89
Nibin Joseph <nibin.joseph.career@gmail.com>
2026-04-13 21:20:55 +0530

    initial commit : added spring poject and connected with database

---
+++ src/main/java/com/nibin/fintech/FintechApiApplication.java
@@ -0,0 +1,13 @@
+package com.nibin.fintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nibin/fintech/FintechApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nibin.fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
