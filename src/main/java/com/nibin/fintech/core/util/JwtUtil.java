0107ea8f6b6e9eef758d27dbefed8c1341ddf81f
Nibin Joseph <nibin.joseph.career@gmail.com>
2026-04-14 21:39:41 +0530

    added global exception handler to handle internal server error and all, also added the apiexception to handle the errors

--- src/main/java/com/nibin/fintech/core/util/JwtUtil.java
+++ src/main/java/com/nibin/fintech/core/util/JwtUtil.java
@@ -2,6 +2,7 @@ package com.nibin.fintech.core.util;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -10,17 +11,21 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    private final String SECRET = "mysecretkeymysecretkeymysecretkey";
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
 
     private Key getKey() {
-        return Keys.hmacShaKeyFor(SECRET.getBytes());
+        return Keys.hmacShaKeyFor(secret.getBytes());
     }
 
     public String generateToken(String email) {
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(getKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
--- src/main/java/com/nibin/fintech/modules/auth/service/AuthService.java
+++ src/main/java/com/nibin/fintech/modules/auth/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.nibin.fintech.modules.auth.service;
 
+import com.nibin.fintech.core.exception.ApiException;
 import com.nibin.fintech.core.util.JwtUtil;
 import com.nibin.fintech.modules.user.entity.User;
 import com.nibin.fintech.modules.user.entity.enums.Status;
@@ -13,20 +14,22 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     private final UserRepository userRepository;
+
     private final PasswordEncoder passwordEncoder;
+
     private final JwtUtil jwtUtil;
 
     public String login(String email, String password) {
 
         User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+                .orElseThrow(() -> new ApiException("User not found"));
 
         if (user.getStatus() != Status.ACTIVE) {
-            throw new RuntimeException("User not verified");
+            throw new ApiException("User not verified");
         }
 
         if (!passwordEncoder.matches(password, user.getPassword())) {
-            throw new RuntimeException("Invalid credentials");
+            throw new ApiException("Invalid credentials");
         }
 
         return jwtUtil.generateToken(user.getEmail());
